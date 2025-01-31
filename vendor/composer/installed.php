f3236bb0a1016c36169135af9eb325a5478f3348
phantoone <aniazulita@gmail.com>
2025-01-31 18:15:49 +0100

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aniaz/ejercicio-php',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aniaz/ejercicio-php' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tourcms/tourcms-php' => array(
+            'pretty_version' => 'v4.0.0',
+            'version' => '4.0.0.0',
+            'reference' => 'e4907f07d18e62bc7112b126cc8cd7d0fb53553e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tourcms/tourcms-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
